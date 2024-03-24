27346962a0fc62e9ac8f49943aed26102afda8f0
TerminalVibes <87617218+TerminalVibes@users.noreply.github.com>
2024-03-24 18:57:42 -0400

    Update AutoUpdater.lua

--- AutoUpdater.lua
+++ AutoUpdater.lua
@@ -1,4 +1,4 @@
 -- https://github.com/Herrtt/AimHot-v8
 pcall(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Herrtt/AimHot-v8/master/Main.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TerminalVibes/AimHot-v8/master/Main.lua", true))()
 end)
